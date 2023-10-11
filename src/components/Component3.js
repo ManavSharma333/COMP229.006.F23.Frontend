045210a5e257ebcca558415f8092d59e70a59671
JViniX <47263194+JViniX@users.noreply.github.com>
2023-10-10 22:20:10 -0400

    Week06 - React Forms

--- src/components/Component3.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react';
-import { CountContext } from './UseContextExample';
-
-export default function Component3() {
-  const count = useContext(CountContext);
-
-  return <>Value: {count}</>
-}
--- src/index.js
+++ src/index.js
@@ -1,24 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-import UseStateExample from './components/UseStateExample';
-import Counter from './components/UseReducerExample';
-import UseContextExample from './components/UseContextExample';
-import ComponentC from './components/ComponentC';
-import Component3 from './components/Component3';
-import StarRating from './components/StarRating';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <UseStateExample />
-    <p>Test</p>
-    <Counter/>
-    <UseContextExample Component3={Component3} ComponentC={ ComponentC}/>
-    <br/><br/>
-    <StarRating/>
+    <App />
   </React.StrictMode>
 );
 
